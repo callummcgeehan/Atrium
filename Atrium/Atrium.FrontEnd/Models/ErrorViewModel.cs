b7fb06c977b3db0c04d6dccb95f0f498973bdf97
callummcgeehan <callum.mcgeehan@fernsoftware.com>
2018-09-25 15:15:00 +0100

    Atrium

---
+++ Atrium/Atrium.FrontEnd/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Atrium.FrontEnd.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
